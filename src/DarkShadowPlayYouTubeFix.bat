799fce77a5a1fd569eb31dcb472ad0957e9915bc
MKeefeus <27903055+MKeefeus@users.noreply.github.com>
2021-01-01 15:50:23 -0500

    Added trim functionality
    Added trim ability, assumed ffmpeg added to path for clarity. Updated README

--- src/DarkShadowPlayYouTubeFix.bat
+++ src/DarkShadowPlayYouTubeFix.bat
@@ -1,5 +1,9 @@
-echo off
+@echo off
 set arg1=%1
-"%~dp0ffmpeg"  -i %arg1% -bsf:v h264_metadata=video_full_range_flag=1 -c:v copy -c:a copy  "%~dp1___temp___.mp4"
+set /P begin="Begin Timestamp: "
+Set /P end="End Timestamp: "
+ffmpeg -i %arg1% -ss %begin% -to %end% -c:v copy -c:a copy "%~dp1___temp___.mp4"
+ffmpeg -i "%~dp1___temp___.mp4" -bsf:v h264_metadata=video_full_range_flag=1 -c:v copy -c:a copy "%~dp1___temp2___.mp4"
 del %arg1%
-ren "%~dp1___temp___.mp4" "%~n1%~x1"
+del "%~dp1___temp___.mp4"
+ren "%~dp1___temp2___.mp4" "%~n1%~x1"
