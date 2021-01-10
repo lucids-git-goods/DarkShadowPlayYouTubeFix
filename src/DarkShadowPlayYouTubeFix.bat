d6f74cb9af50940c0d15f1dc6dd856bf3c0bb086
lucid <edgaromuniz@gmail.com>
2021-01-10 13:03:47 -0500

    Fixed bug that would occur when a new conversion was initiated before the previous had completed

--- src/DarkShadowPlayYouTubeFix.bat
+++ src/DarkShadowPlayYouTubeFix.bat
@@ -7,10 +7,15 @@ if exist "%~dp0ffmpeg.exe" (
 	set ff="ffmpeg"
 )
 
-%ff%  -i %arg1% -bsf:v h264_metadata=video_full_range_flag=1 -c:v copy -c:a copy  "%~dp1___temp___.mp4"
+set fileSuffix=-1
+:fileNameLoop
+set /a fileSuffix+=1
+if exist "%~dp1___temp___%fileSuffix%.mp4" goto fileNameLoop
+
+%ff%  -i %arg1% -bsf:v h264_metadata=video_full_range_flag=1 -c:v copy -c:a copy  "%~dp1___temp___%fileSuffix%.mp4"
 IF NOT %ERRORLEVEL%==9009 ( 
 del %arg1%
-ren "%~dp1___temp___.mp4" "%~n1%~x1"
+ren "%~dp1___temp___%fileSuffix%.mp4" "%~n1%~x1"
 ) else (
 ECHO error : missing ffmpeg.exe
 pause
--- src/DarkShadowPlayYouTubeFixWithTrim.bat
+++ src/DarkShadowPlayYouTubeFixWithTrim.bat
@@ -9,12 +9,23 @@ if exist "%~dp0ffmpeg.exe" (
 	set ff="ffmpeg"
 )
 
-%ff% -i %arg1% -ss %begin% -to %end% -c:v copy -c:a copy "%~dp1___temp___.mp4"
-%ff% -i "%~dp1___temp___.mp4" -bsf:v h264_metadata=video_full_range_flag=1 -c:v copy -c:a copy "%~dp1___temp2___.mp4"
+set fileSuffixA=-1
+:fileNameLoopA
+set /a fileSuffixA+=1
+if exist "%~dp1___temp___%fileSuffixA%.mp4" goto fileNameLoopA
+
+set fileSuffixB=-1
+:fileNameLoopB
+set /a fileSuffixB+=1
+if exist "%~dp1___temp2___%fileSuffixB%.mp4" goto fileNameLoopB
+
+%ff% -i %arg1% -ss %begin% -to %end% -c:v copy -c:a copy "%~dp1___temp___%fileSuffixA%.mp4"
+%ff% -i "%~dp1___temp___%fileSuffixA%.mp4" -bsf:v h264_metadata=video_full_range_flag=1 -c:v copy -c:a copy "%~dp1___temp2___%fileSuffixB%.mp4"
+
 IF NOT %ERRORLEVEL%==9009 (
 del %arg1%
-del "%~dp1___temp___.mp4"
-ren "%~dp1___temp2___.mp4" "%~n1%~x1"
+del "%~dp1___temp___%fileSuffixA%.mp4"
+ren "%~dp1___temp2___%fileSuffixB%.mp4" "%~n1%~x1"
 ) else (
 ECHO error : missing ffmpeg.exe
 pause
